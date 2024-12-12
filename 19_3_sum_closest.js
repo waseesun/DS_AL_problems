979e648a175fd12e1a70a4fae3ea984111616573
Wasee Sun <revengersun@gmail.com>
2024-12-12 15:20:56 +0600

    Removed python boiler code

--- 19_3_sum_closest.js
+++ 19_3_sum_closest.js
@@ -1,33 +1,3 @@
-// class Solution:
-//     def threeSum(self, nums):
-//         nums.sort()  # Sort the list first
-//         lst = []  # List to store the triplets
-
-//         for i in range(len(nums) - 2):
-//             if i > 0 and nums[i] == nums[i - 1]:
-//                 # Skip duplicates
-//                 continue
-
-//             left, right = i + 1, len(nums) - 1
-//             while left < right:
-//                 total = nums[i] + nums[left] + nums[right]
-
-//                 if total == 0:
-//                     lst.append([nums[i], nums[left], nums[right]])
-//                     # Move the pointers to skip duplicates
-//                     while left < right and nums[left] == nums[left + 1]:
-//                         left += 1
-//                     while left < right and nums[right] == nums[right - 1]:
-//                         right -= 1
-//                     left += 1
-//                     right -= 1
-//                 elif total < 0:
-//                     left += 1
-//                 else:
-//                     right -= 1
-
-//         return lst
-
 /**
  * @param {number[]} nums
  * @param {number} target
